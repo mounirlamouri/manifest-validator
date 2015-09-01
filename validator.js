6ed45cead07828810b8a5cdf9e61efe86d2a5816
Mounir Lamouri <mounir@lamouri.fr>
2015-09-01 21:11:49 +0100

    Add 'start_url' with the exception of URL parsing.

--- validator.js
+++ validator.js
@@ -5,7 +5,7 @@ var ManifestValidator = (function() {
   var _manifest = {};
   var _logs = [];
 
-  function _checkName(manifest) {
+  function _checkName() {
     if (!_manifest.name) {
       _logs.push('Manifest has no `name` property.')
       return;
@@ -20,7 +20,7 @@ var ManifestValidator = (function() {
     _logs.push('Parsed `name` property is: ' + _manifest.name);
   }
 
-  function _checkShortName(manifest) {
+  function _checkShortName() {
     if (!_manifest.short_name) {
       _logs.push('Manifest has no `short_name` property.')
       return;
@@ -35,6 +35,21 @@ var ManifestValidator = (function() {
     _logs.push('Parsed `short_name` property is: ' + _manifest.short_name);
   }
 
+  function _checkStartUrl() {
+    if (!_manifest.start_url) {
+      _logs.push('Manifest has no `start_url` property.');
+      return;
+    }
+
+    if (typeof _manifest.start_url != "string") {
+      _logs.push('ERROR: Manifest\'s `short_name` property type is invalid.');
+      return;
+    }
+
+    // TODO: parse url using manifest_url as a base (missing).
+    _logs.push('Parsed `start_url` property is: ' + _manifest.start_url);
+  }
+
   function _check(string) {
     try {
       _manifest = JSON.parse(string);
@@ -47,6 +62,7 @@ var ManifestValidator = (function() {
 
     _checkName();
     _checkShortName();
+    _checkStartUrl();
 
     return [ true, _logs ];
   }
