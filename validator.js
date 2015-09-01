f2a2f4a173bebf87f5ff150ed47bd0385938c5dc
Mounir Lamouri <mounir@lamouri.fr>
2015-09-02 00:31:13 +0100

    Some refactoring.

--- validator.js
+++ validator.js
@@ -2,57 +2,41 @@
 
 var ManifestValidator = (function() {
 
+  var _json_input = {};
   var _manifest = {};
   var _logs = [];
 
-  function _checkName() {
-    if (!_manifest.name) {
-      _logs.push('Manifest has no `name` property.')
-      return;
+  function _parseString(args) {
+    var property = args.property;
+    if (!(property in _json_input))
+      return undefined;
+
+    if (typeof _json_input[property] != 'string') {
+      _logs.push('ERROR: "' + property + '" expected to be a string but is not.');
+      return undefined;
     }
 
-    if (typeof _manifest.name != "string") {
-      _logs.push('ERROR: Manifest\'s `name` property type is invalid.');
-      return;
-    }
-
-    _manifest.name = _manifest.name.trim();
-    _logs.push('Parsed `name` property is: ' + _manifest.name);
+    if (args.trim)
+      return _json_input[property].trim();
+    return _json_input[property];
   }
 
-  function _checkShortName() {
-    if (!_manifest.short_name) {
-      _logs.push('Manifest has no `short_name` property.')
-      return;
-    }
-
-    if (typeof _manifest.short_name != "string") {
-      _logs.push('ERROR: Manifest\'s `short_name` property type is invalid.');
-      return;
-    }
-
-    _manifest.short_name = _manifest.short_name.trim();
-    _logs.push('Parsed `short_name` property is: ' + _manifest.short_name);
+  function _parseName() {
+    return _parseString({ property: 'name', trim: true });
   }
 
-  function _checkStartUrl() {
-    if (!_manifest.start_url) {
-      _logs.push('Manifest has no `start_url` property.');
-      return;
-    }
+  function _parseShortName() {
+    return _parseString({ property: 'short_name', trim: true });
+  }
 
-    if (typeof _manifest.start_url != "string") {
-      _logs.push('ERROR: Manifest\'s `short_name` property type is invalid.');
-      return;
-    }
-
+  function _parseStartUrl() {
     // TODO: parse url using manifest_url as a base (missing).
-    _logs.push('Parsed `start_url` property is: ' + _manifest.start_url);
+    return _parseString({ property: 'start_url', trim: false });
   }
 
   function _check(string) {
     try {
-      _manifest = JSON.parse(string);
+      _json_input = JSON.parse(string);
     } catch (e) {
       _logs.push("File isn't valid JSON: " + e);
       return [ false, _logs ];
@@ -60,9 +44,13 @@ var ManifestValidator = (function() {
 
     _logs.push("JSON parsed successfuly.");
 
-    _checkName();
-    _checkShortName();
-    _checkStartUrl();
+    _manifest.name = _parseName();
+    _manifest.short_name = _parseShortName();
+    _manifest.start_url= _parseStartUrl();
+
+    _logs.push('Parsed `name` property is: ' + _manifest.name);
+    _logs.push('Parsed `short_name` property is: ' + _manifest.short_name);
+    _logs.push('Parsed `start_url` property is: ' + _manifest.start_url);
 
     return [ true, _logs ];
   }
