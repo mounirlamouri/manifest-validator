f73e44f2be8ea899c4d58d7b1a18a7149ec05a6a
Sam Saccone <samccone@gmail.com>
2015-12-11 21:03:26 -0800

    JSCS manifest
    * Also add CI tests to ensure we continue to conform :)

--- manifest-parser.js
+++ manifest-parser.js
@@ -1,38 +1,41 @@
 var ManifestParser = (function() {
-  "use strict";
+  'use strict';
 
-  var _json_input = {};
+  var _jsonInput = {};
   var _manifest = {};
   var _logs = [];
   var _success = true;
 
-  var ALLOWED_DISPLAY_VALUES = [ 'fullscreen',
+  var ALLOWED_DISPLAY_VALUES = ['fullscreen',
                                  'standalone',
                                  'minimal-ui',
-                                 'browser' ];
+                                 'browser'];
 
-  var ALLOWED_ORIENTATION_VALUES = [ 'any',
+  var ALLOWED_ORIENTATION_VALUES = ['any',
                                      'natural',
                                      'landscape',
                                      'portrait',
                                      'portrait-primary',
                                      'portrait-secondary',
                                      'landscape-primary',
-                                     'landscape-secondary' ];
+                                     'landscape-secondary'];
 
   function _parseString(args) {
     var object = args.object;
     var property = args.property;
-    if (!(property in object))
-      return undefined;
-
-    if (typeof object[property] != 'string') {
-      _logs.push('ERROR: "' + property + '" expected to be a string but is not.');
+    if (!(property in object)) {
       return undefined;
     }
 
-    if (args.trim)
+    if (typeof object[property] != 'string') {
+      _logs.push('ERROR: \'' + property +
+          '\' expected to be a string but is not.');
+      return undefined;
+    }
+
+    if (args.trim) {
       return object[property].trim();
+    }
     return object[property];
   }
 
@@ -40,11 +43,13 @@ var ManifestParser = (function() {
     var object = args.object;
     var property = args.property;
     var defaultValue = args.defaultValue;
-    if (!(property in object))
+    if (!(property in object)) {
       return defaultValue;
+    }
 
     if (typeof object[property] != 'boolean') {
-      _logs.push('ERROR: "' + property + '" expected to be a boolean but is not.');
+      _logs.push('ERROR: \'' + property +
+          '\' expected to be a boolean but is not.');
       return defaultValue;
     }
 
@@ -56,9 +61,10 @@ var ManifestParser = (function() {
     var property = args.property;
     var baseURL = args.baseURL;
 
-    var str = _parseString({ object: object, property: property, trim: false });
-    if (str === undefined)
+    var str = _parseString({object: object, property: property, trim: false});
+    if (str === undefined) {
       return undefined;
+    }
 
     // TODO: resolve url using baseURL
     // new URL(object[property], baseURL);
@@ -68,11 +74,13 @@ var ManifestParser = (function() {
   function _parseColor(args) {
     var object = args.object;
     var property = args.property;
-    if (!(property in object))
+    if (!(property in object)) {
       return undefined;
+    }
 
     if (typeof object[property] != 'string') {
-      _logs.push('ERROR: "' + property + '" expected to be a string but is not.');
+      _logs.push('ERROR: \'' + property +
+          '\' expected to be a string but is not.');
       return undefined;
     }
 
@@ -81,39 +89,42 @@ var ManifestParser = (function() {
     var dummy = document.createElement('div');
     dummy.style.color = 'white';
     dummy.style.color = object[property];
-    if (dummy.style.color != 'white')
+    if (dummy.style.color != 'white') {
       return object[property];
+    }
     dummy.style.color = 'black';
     dummy.style.color = object[property];
-    if (dummy.style.color != 'black')
+    if (dummy.style.color != 'black') {
       return object[property];
+    }
     return undefined;
   }
 
   function _parseName() {
-    return _parseString({ object: _json_input, property: 'name', trim: true });
+    return _parseString({object: _jsonInput, property: 'name', trim: true});
   }
 
   function _parseShortName() {
-    return _parseString({ object: _json_input,
+    return _parseString({object: _jsonInput,
                           property: 'short_name',
-                          trim: true });
+                          trim: true});
   }
 
   function _parseStartUrl() {
     // TODO: parse url using manifest_url as a base (missing).
-    return _parseURL({ object: _json_input, property: 'start_url' });
+    return _parseURL({object: _jsonInput, property: 'start_url'});
   }
 
   function _parseDisplay() {
-    var display = _parseString({ object: _json_input,
+    var display = _parseString({object: _jsonInput,
                                  property: 'display',
-                                 trim: true });
-    if (display === undefined)
+                                 trim: true});
+    if (display === undefined) {
       return display;
+    }
 
     if (ALLOWED_DISPLAY_VALUES.indexOf(display.toLowerCase()) == -1) {
-      _logs.push('ERROR: "display" has an invalid value, will be ignored.');
+      _logs.push('ERROR: \'display\' has an invalid value, will be ignored.');
       return undefined;
     }
 
@@ -121,14 +132,16 @@ var ManifestParser = (function() {
   }
 
   function _parseOrientation() {
-    var orientation = _parseString({ object: _json_input,
+    var orientation = _parseString({object: _jsonInput,
                                      property: 'orientation',
-                                     trim: true });
-    if (orientation === undefined)
+                                     trim: true});
+    if (orientation === undefined) {
       return orientation;
+    }
 
     if (ALLOWED_ORIENTATION_VALUES.indexOf(orientation.toLowerCase()) == -1) {
-      _logs.push('ERROR: "orientation" has an invalid value, will be ignored.');
+      _logs.push('ERROR: \'orientation\' has an invalid value' +
+          ', will be ignored.');
       return undefined;
     }
 
@@ -139,38 +152,44 @@ var ManifestParser = (function() {
     var property = 'icons';
     var icons = [];
 
-    if (!(property in _json_input))
-      return icons;
-
-    if (!Array.isArray(_json_input[property])) {
-      _logs.push('ERROR: "' + property + '" expected to be an array but is not.');
+    if (!(property in _jsonInput)) {
       return icons;
     }
 
-    _json_input[property].forEach(function(object) {
-      var icon = {};
-      if (!('src' in object))
-        return;
-      // TODO: pass manifest url as base.
-      icon.src = _parseURL({ object: object, property: 'src' });
-      icon.type = _parseString({ object: object,
-                                 property: 'type',
-                                 trim: true });
+    if (!Array.isArray(_jsonInput[property])) {
+      _logs.push('ERROR: \'' + property +
+          '\' expected to be an array but is not.');
+      return icons;
+    }
 
-      icon.density = parseFloat(object['density']);
-      if (isNaN(icon.density) || !isFinite(icon.density) || icon.density <= 0)
+    _jsonInput[property].forEach(function(object) {
+      var icon = {};
+      if (!('src' in object)) {
+        return;
+      }
+      // TODO: pass manifest url as base.
+      icon.src = _parseURL({object: object, property: 'src'});
+      icon.type = _parseString({object: object,
+                                 property: 'type',
+                                 trim: true});
+
+      icon.density = parseFloat(object.density);
+      if (isNaN(icon.density) || !isFinite(icon.density) || icon.density <= 0) {
         icon.density = 1.0;
+      }
 
       if ('sizes' in object) {
         var set = new Set();
         var link = document.createElement('link');
-        link.sizes = object['sizes'];
+        link.sizes = object.sizes;
 
-        for (var i = 0; i < link.sizes.length; ++i)
+        for (var i = 0; i < link.sizes.length; ++i) {
           set.add(link.sizes.item(i).toLowerCase());
+        }
 
-        if (set.size != 0)
+        if (set.size != 0) {
           icon.sizes = set;
+        }
       }
 
       icons.push(icon);
@@ -183,24 +202,26 @@ var ManifestParser = (function() {
     var property = 'related_applications';
     var applications = [];
 
-    if (!(property in _json_input))
-      return applications;
-
-    if (!Array.isArray(_json_input[property])) {
-      _logs.push('ERROR: "' + property + '" expected to be an array but is not.');
+    if (!(property in _jsonInput)) {
       return applications;
     }
 
-    _json_input[property].forEach(function(object) {
+    if (!Array.isArray(_jsonInput[property])) {
+      _logs.push('ERROR: \'' + property +
+          '\' expected to be an array but is not.');
+      return applications;
+    }
+
+    _jsonInput[property].forEach(function(object) {
       var application = {};
-      application.platform = _parseString({ object: object,
+      application.platform = _parseString({object: object,
                                             property: 'platform',
-                                            trim: true });
-      application.id = _parseString({ object: object,
+                                            trim: true});
+      application.id = _parseString({object: object,
                                       property: 'id',
-                                      trim: true });
+                                      trim: true});
       // TODO: pass manfiest url as base.
-      application.url = _parseURL({ object: object, property: 'url' });
+      application.url = _parseURL({object: object, property: 'url'});
       applications.push(application);
     });
 
@@ -208,17 +229,17 @@ var ManifestParser = (function() {
   }
 
   function _parsePreferRelatedApplications() {
-    return _parseBoolean({ object: _json_input,
+    return _parseBoolean({object: _jsonInput,
                            property: 'prefer_related_applications',
-                           defaultValue: false });
+                           defaultValue: false});
   }
 
   function _parseThemeColor() {
-    return _parseColor({ object: _json_input, property: 'theme_color' });
+    return _parseColor({object: _jsonInput, property: 'theme_color'});
   }
 
   function _parseBackgroundColor() {
-    return _parseColor({ object: _json_input, property: 'background_color' });
+    return _parseColor({object: _jsonInput, property: 'background_color'});
   }
 
   function _parse(string) {
@@ -227,18 +248,19 @@ var ManifestParser = (function() {
     _success = true;
 
     try {
-      _json_input = JSON.parse(string);
+      _jsonInput = JSON.parse(string);
     } catch (e) {
-      _logs.push("File isn't valid JSON: " + e);
+      _logs.push('File isn\'t valid JSON: ' + e);
       _success = false;
       return;
     }
 
-    _logs.push("JSON parsed successfuly.");
+    _logs.push('JSON parsed successfuly.');
 
     _manifest.name = _parseName();
+    //jscs:disable
     _manifest.short_name = _parseShortName();
-    _manifest.start_url= _parseStartUrl();
+    _manifest.start_url = _parseStartUrl();
     _manifest.display = _parseDisplay();
     _manifest.orientation = _parseOrientation();
     _manifest.icons = _parseIcons();
@@ -257,6 +279,7 @@ var ManifestParser = (function() {
     _logs.push('Parsed `prefer_related_applications` property is: ' + _manifest.prefer_related_applications);
     _logs.push('Parsed `theme_color` property is: ' + _manifest.theme_color);
     _logs.push('Parsed `background_color` property is: ' + _manifest.background_color);
+    //jscs:enable
   }
 
   return {
